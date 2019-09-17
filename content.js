301d6efedc6535d1ffbc49af0a9db56bf9769730
Robert Greliński <robert.grelinski@student.put.poznan.pl>
2019-09-17 19:28:39 +0200

    Dodanie gołych panów

--- content.js
+++ content.js
@@ -1,4 +1,20 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
-}
+let sexiImg=[
+  "https://images.sex.com/images/pinporn/2015/05/17/620/11956297.gif",
+  "https://66.media.tumblr.com/021c63ae7422b362563abd861763d6fb/tumblr_phyq8cPQ4L1xqr2fio1_540.gif",
+  "https://images.sex.com/images/pinporn/2016/01/14/620/14780313.gif",
+  "https://images.sex.com/images/pinporn/2014/11/05/620/8939122.gif",
+  "https://images.sex.com/images/pinporn/2018/08/09/620/19821153.gif",
+  "https://images.sex.com/images/pinporn/2018/11/03/620/20167787.gif",
+  "https://images.sex.com/images/pinporn/2018/09/14/620/19957390.gif",
+  "https://images.sex.com/images/pinporn/2016/11/07/620/16866488.gif",
+  "https://images.sex.com/images/pinporn/2015/06/04/620/12214536.gif",
+  "https://images.sex.com/images/pinporn/2017/03/07/620/17455393.gif",
+  "https://images.sex.com/images/pinporn/2015/05/17/620/11956297.gif",
+  "https://images.sex.com/images/pinporn/2017/06/07/620/17874801.gif",
+  "https://images.sex.com/images/pinporn/2016/02/20/620/15041534.gif"
+]
+$("img").each(function(){
+  $(this).attr("src",sexiImg[Math.floor(Math.random() * sexiImg.length)])
+})
+
+
